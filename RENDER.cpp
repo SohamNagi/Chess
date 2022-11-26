10aa173fc0b15788373cdf4755c7f42f7f145424
s2nagi <s2nagi@uwaterloo.ca>
2022-11-25 20:48:21 -0500

    Added Documentation

--- RENDER.cpp
+++ RENDER.cpp
@@ -1,29 +1,37 @@
-#include iostream
-#include string
-#include vector
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-vector<char> fenToBoard(string input){
+char** fenToBoard(string input){
     int row = 7;
     int col = 0;
     int i = 0;
-    vector<char> board(64, ' ');
+    char** board = new char*[8];
+    for (int h = 0; h < 8; h++){
+        board[h] = new char[8];
+        for (int w = 0; w < 8; w++){
+            board[h][w] = ' ';
+        }
+    }
     while (input[i] != ' ') {
         char curr = input[i];
-        if (curr == /){
+        if (curr == '/'){
             row--;
         } else if (isdigit(curr)) {
             col += curr;
         } else {
-            board[i][j] = curr;
+            board[row][col] = curr;
+            col++;
         }
-        i++
+        i++;
     }
-    return board;
+    char** out = board;
+    return out;
 }
 
-void printBoard(vector<char> Board){
-    cout << "A B C D E F G H"
+void printBoard(char** Board){
+    cout << "A B C D E F G H";
     for(int i = 0; i < 8; i ++){
         cout << i+1 << " ";
         for(int j = 0; j < 8; j++){
@@ -43,7 +51,7 @@ void printBoard(vector<char> Board){
 }
 
 int main(int argc, const char** argv) {
-    string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     vector<char> board = fenToBoard(fen);
     for(char i: board){
         cout << i;
