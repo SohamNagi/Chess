515a003e01b1baa1fa41744c563236ba145ccd4d
Ariq Ishfar <ariq.ishfar@uwaterloo.ca>
2022-11-25 21:16:51 -0500

    Basic Board header file base on UML

---
+++ board.h
@@ -0,0 +1,40 @@
+#ifndef BOARDSTATE.H
+#define BOARDSTATE.H
+
+#include <vector>
+#include <string>
+#include "coord.h"
+
+class Pieces;
+
+class BoardState{
+
+    // Fields for Board
+    public:
+
+        std::vector<Pieces> board;
+        std::vector<Coord> LastMove;
+        Coord en_passant;
+
+    private:
+
+        int halfMoves;
+        int moves;
+        bool WhiteCheck;
+        bool BlackCheck;
+
+    // Methods for Board:
+    public:
+        void attachPiece();
+        void detachPiece();
+
+    private:
+
+        std::string FEN();
+        void notifyStateChange();
+};
+
+#endif
+
+
+
