0258668e4487d6c9527483fdbdb35f9a86bc676f
Abdullah <a38shahi@linux.student.cs.uwaterloo.ca>
2022-11-28 22:21:16 -0500

    made all methods in piece.h public

--- pieces.h
+++ pieces.h
@@ -19,41 +19,48 @@ class Pieces {
 };
 
 class Pawn : public Pieces {
-    bool moved;
-    virtual void updateMoves() override;
-    Pawn(Board* board, bool isWhite, int location, char type;);
+    public:
+        bool moved;
+        virtual void updateMoves() override;
+        Pawn(Board* board, bool isWhite, int location, char type;);
 };
 
 class Rook: public Pieces {
-    bool moved;
-    virtual void updateMoves() override;
-    Rook(Board* board, bool isWhite, int location, char type;);
+    public:
+        bool moved;
+        virtual void updateMoves() override;
+        Rook(Board* board, bool isWhite, int location, char type;);
 };
 
 class King: public Pieces {
-    bool moved;
-    virtual void updateMoves() override;
-    King(Board* board, bool isWhite, int location, char type;);
+    public:
+        bool moved;
+        virtual void updateMoves() override;
+        King(Board* board, bool isWhite, int location, char type;);
 };
 
 class Queen: public Pieces {
-    virtual void updateMoves() override;
-    Queen(Board* board, bool isWhite, int location, char type;);
+    public:
+        virtual void updateMoves() override;
+        Queen(Board* board, bool isWhite, int location, char type;);
 };
 
 class Knight: public Pieces {
-    virtual void updateMoves() override;
-    Knight(Board* board, bool isWhite, int location, char type;);
+    public:
+        virtual void updateMoves() override;
+        Knight(Board* board, bool isWhite, int location, char type;);
 };
 
 class Bishop: public Pieces {
-    virtual void updateMoves() override;
-    Bishop(Board* board, bool isWhite, int location, char type;)
+    public:
+        virtual void updateMoves() override;
+        Bishop(Board* board, bool isWhite, int location, char type;)
 };
 
 class emptyPiece: public Pieces {
-    virtual void updateMoves() override;
-    emptyPiece(Board* board, int location);
+    public:
+        virtual void updateMoves() override;
+        emptyPiece(Board* board, int location);
 };
 
 #endif
