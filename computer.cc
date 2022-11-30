f8c4ac4cf6db7659339713a31b4eb3715dfe6b18
Ariq Ishfar <aishfar@uwaterloo.ca>
2022-11-29 22:40:33 -0500

    Added constructors for computer classes

--- computer.cc
+++ computer.cc
@@ -7,3 +7,11 @@ void Computer::getmove() {
     BestMove move = this->evaluate();
     this->move(move.start, move.end, this->board);
 }
+
+Level1::Level1(bool isWhite, Board* board): Computer(isWhite, board) {};
+
+Level2::Level2(bool isWhite, Board* board): Computer(isWhite, board) {};
+
+Level3::Level3(bool isWhite, Board* board): Computer(isWhite, board) {};
+
+Level4::Level4(bool isWhite, Board* board): Computer(isWhite, board) {};
--- computer.h
+++ computer.h
@@ -20,18 +20,22 @@ class Computer: public Player {
 };
 
 class Level1: public Computer {
+    Level1(bool isWhite, Board* board);
     BestMove evaluate();
 };
 
 class Level2: public Computer {
+    Level2(bool isWhite, Board* board);
     BestMove evaluate();
 };
 
 class Level3: public Computer {
+    Level3(bool isWhite, Board* board);
     BestMove evaluate();
 };
 
 class Level4: public Computer {
+    Level4(bool isWhite, Board* board);
     BestMove evaluate();
 };
 
