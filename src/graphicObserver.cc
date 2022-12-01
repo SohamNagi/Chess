8529a9b7531543ec35e9df5a65d055ff598e7ac0
s2nagi <s2nagi@uwaterloo.ca>
2022-11-30 23:56:39 -0500

    Imporved GFx

--- src/graphicObserver.cc
+++ src/graphicObserver.cc
@@ -7,23 +7,25 @@ graphicObserver::graphicObserver(Game* sub):
     win{new Xwindow(500,500)}, subject{sub}
 {
     std::string header = "CS246 - C++ Chess";
-    win->drawString(200, 35, header);
+    win->drawString(90, 35, header);
     grid.assign(64, ' ');
     int shift = 50;
+    char file = 'A';
     for(int i = 0; i < 8; i++){
-        win->drawString(25, i*50 + 25 + shift, std::to_string(8-i));
+        win->drawString(20, i*50 + 35 + shift, std::to_string(8-i));
         for(int j = 0; j < 8; j++){
             if ((j+i) % 2 != 0){
                 win->fillRectangle((i*50) + shift, (j*50) + shift, 50, 50, 4); // Print Black Square
             }
         }
+        std::string s(1,file);
+        win->drawString((i*50) + 10 + shift, 490, s);
+        file++;
     }
     win->fillRectangle(shift, shift, 400, 5, 1);
     win->fillRectangle(shift, shift, 5, 400, 1);
     win->fillRectangle(400+shift, shift, 5, 405, 1);
     win->fillRectangle(shift, 400+shift, 400, 5, 1);
-    std::string rows = "A       B       C        D       E        F       G       H";
-    win->drawString(25 + shift, 400 + 25 + shift, rows);
 };
 
 graphicObserver::~graphicObserver(){
@@ -44,7 +46,7 @@ void graphicObserver::notify(){
                 }
                 grid[index] = curr; // Reset Value in Optimization index
                 std::string s(1, curr);
-                win->drawString(i*50 + 25 + shift, j*50 + 25 + shift, s); // Print Piece Name
+                win->drawString(i*50 + 15 + shift, j*50 + 35 + shift, s); // Print Piece Name
             }
         }
     }
