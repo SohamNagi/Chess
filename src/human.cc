a9efc400e6182a26236d8571bc45b1f531e05e7b
s2nagi <s2nagi@uwaterloo.ca>
2022-12-02 02:42:14 -0500

    fixed exceptions!

--- src/human.cc
+++ src/human.cc
@@ -24,31 +24,27 @@ void Human::getmove() {
         cin >> input_c;
 
         if(cin.fail()) {
-            cerr << "Unable to read starting column. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Unable to read ending column. Try again.");
         }
 
         if (('a' <= input_c) && (input_c <= 'h')) {
             start = (input_c - 'a');     
  
         } else {
-            cerr << "Invalid starting column. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Invalid ending column. Try again.");
         }
 
         cin >> input_n;
 
         if(cin.fail()) {
-            cerr << "Unable to read starting row. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Unable to read ending row. Try again.");
         }
 
         if ((1 <= input_n) && (input_n <= 8)) {
             start += (input_n - 1) * 8;
  
         } else {
-            cerr << "Invalid starting row. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Invalid ending row. Try again.");
         }
 
         // Reading end position
@@ -56,31 +52,27 @@ void Human::getmove() {
         cin >> input_c;
 
         if(cin.fail()) {
-            cerr << "Unable to read ending column. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Unable to read ending column. Try again.");
         }
 
         if (('a' <= input_c) && (input_c <= 'h')) {
             end = (input_c - 'a');
  
         } else {
-            cerr << "Invalid ending column. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Invalid ending column. Try again.");
         }
 
         cin >> input_n;
 
         if(cin.fail()) {
-            cerr << "Unable to read ending row. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Unable to read ending row. Try again.");
         }
 
         if ((1 <= input_n) && (input_n <= 8)) {
             end += (input_n - 1) * 8;
  
         } else {
-            cerr << "Invalid starting row. Try again." << endl;
-            throw "Entry";
+            throw std::invalid_argument("Invalid ending row. Try again.");
         }
 
         move(start, end, board);
