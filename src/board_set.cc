adddd69082f34cc24600272fd09730ac3258177c
SohamNagi <nagisoham@gmail.com>
2022-12-06 04:42:42 -0500

    Setup now allows for castling

--- src/board_set.cc
+++ src/board_set.cc
@@ -11,27 +11,27 @@ using namespace std;
 // Board setup returns a pointer to a board object by taking user input
 Board* board_setup(vector<char>* start, string set_turn, int status){
     // Creates base graphic window
-    // auto win = new Xwindow{500,500};
-    // win->drawStringBold(90, 35, "CS246 - C++ Chess");
-    // vector<char> opti;
-    // opti.assign(64, ' ');
-    // char file = 'A';
-    // int shift = 50;
-    // for(int i = 0; i < 8; i++){
-    //     win->drawStringBold(20, i*50 + 35 + shift, std::to_string(8-i));
-    //     for(int j = 0; j < 8; j++){
-    //         if ((j+i) % 2 != 0){
-    //             win->fillRectangle((i*50) + shift, (j*50) + shift, 50, 50, 4); // Print Black Square
-    //         }
-    //     }
-    //     std::string s(1,file);
-    //     win->drawStringBold((i*50) + 10 + shift, 490, s);
-    //     file++;
-    // }
-    // win->fillRectangle(shift, shift, 400, 5, 1);
-    // win->fillRectangle(shift, shift, 5, 400, 1);
-    // win->fillRectangle(400+shift, shift, 5, 405, 1);
-    // win->fillRectangle(shift, 400+shift, 400, 5, 1);
+    auto win = new Xwindow{500,500};
+    win->drawStringBold(90, 35, "CS246 - C++ Chess");
+    vector<char> opti;
+    opti.assign(64, ' ');
+    char file = 'A';
+    int shift = 50;
+    for(int i = 0; i < 8; i++){
+        win->drawStringBold(20, i*50 + 35 + shift, std::to_string(8-i));
+        for(int j = 0; j < 8; j++){
+            if ((j+i) % 2 != 0){
+                win->fillRectangle((i*50) + shift, (j*50) + shift, 50, 50, 8); // Print Black Square
+            }
+        }
+        std::string s(1,file);
+        win->drawStringBold((i*50) + 10 + shift, 490, s);
+        file++;
+    }
+    win->fillRectangle(shift, shift, 400, 5, 1);
+    win->fillRectangle(shift, shift, 5, 400, 1);
+    win->fillRectangle(400+shift, shift, 5, 405, 1);
+    win->fillRectangle(shift, 400+shift, 400, 5, 1);
 
     // Initialse Variables For Board Construction
     string command;
@@ -60,7 +60,7 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
             int row; std::cin >> row;
             grid[(8*(row-1))+mapFiles(file)] = piece;
             txt_printer(grid);
-            //gfx_printer(win,grid, &opti);
+            gfx_printer(win,grid, &opti);
         // Remove piece from given index
         } else if (command == "-"){
             char piece; std::cin >> piece;
@@ -68,7 +68,7 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
             int row; std::cin >> row;
             grid[(8*(row-1))+mapFiles(file)] = ' ';
             txt_printer(grid);
-            //gfx_printer(win,grid,&opti);
+            gfx_printer(win,grid,&opti);
         // Set turn
         } else if (command == "="){
             string color; std::cin >> color;
@@ -83,7 +83,7 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
         } else if (command == "fen"){
             std::cin >> output;
             std::cin >> turn;
-            //delete win;
+            delete win;
             output += " ";
             output += turn;
             std::cout << output << endl;
@@ -94,9 +94,44 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
                 std::cout << "Your board is invalid, please fix it!" << std::endl;
                 final_board = board_setup(&grid, turn, -1);
             }
+            #pragma region SET_MOVED
+            // Prevent castling and pawn pushes
             for(auto i: final_board->boardState){
                 i->moved = true;
             }
+            // IF WHITE PAWNS ARE CORRECT
+            for(int i = 8; i < 16; i++){
+                if(final_board->boardState[i]->type == 'P'){
+                    final_board->boardState[i]->moved = false;
+                }
+            }
+            // IF BLACK PAWNS ARE CORRECT
+            for(int i = 48; i < 56; i++){
+                if(final_board->boardState[i]->type == 'p'){
+                    final_board->boardState[i]->moved = false;
+                }
+            }
+            // IF BLACK KING IS CORRECT
+            if(final_board->boardState[60]->type == 'k'){
+                final_board->boardState[60]->moved = false;
+            }
+            // IF WHITE KING IS CORRECT
+            if(final_board->boardState[4]->type == 'K'){
+                final_board->boardState[4]->moved = false;
+            }
+            // IF WHITE ROOKS ARE CORRECT
+            if(final_board->boardState[0]->type == 'R'){
+                final_board->boardState[0]->moved = false;
+            } else if(final_board->boardState[7]->type == 'R'){
+                final_board->boardState[7]->moved = false;
+            }
+            // IF BLACK ROOKS ARE CORRECT
+            if(final_board->boardState[56]->type == 'R'){
+                final_board->boardState[56]->moved = false;
+            } else if(final_board->boardState[63]->type == 'R'){
+                final_board->boardState[63]->moved = false;
+            }
+            #pragma endregion SET_MOVED
             return final_board;
         // Quit setup mode
         } else if (command == "done"){
@@ -128,7 +163,7 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
             output += "/";
         }
     }
-    //delete win;
+    delete win;
     if(output == "8/8/8/8/8/8/8/8"){
         return new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     }
@@ -142,10 +177,45 @@ Board* board_setup(vector<char>* start, string set_turn, int status){
         final_board = board_setup(&grid, turn, -1);
     }
 
+#pragma region SET_MOVED
     // Prevent castling and pawn pushes
     for(auto i: final_board->boardState){
         i->moved = true;
     }
+    // IF WHITE PAWNS ARE CORRECT
+    for(int i = 8; i < 16; i++){
+        if(final_board->boardState[i]->type == 'P'){
+            final_board->boardState[i]->moved = false;
+        }
+    }
+    // IF BLACK PAWNS ARE CORRECT
+    for(int i = 48; i < 56; i++){
+        if(final_board->boardState[i]->type == 'p'){
+            final_board->boardState[i]->moved = false;
+        }
+    }
+    // IF BLACK KING IS CORRECT
+    if(final_board->boardState[60]->type == 'k'){
+        final_board->boardState[60]->moved = false;
+    }
+    // IF WHITE KING IS CORRECT
+    if(final_board->boardState[4]->type == 'K'){
+        final_board->boardState[4]->moved = false;
+    }
+    // IF WHITE ROOKS ARE CORRECT
+    if(final_board->boardState[0]->type == 'R'){
+        final_board->boardState[0]->moved = false;
+    } else if(final_board->boardState[7]->type == 'R'){
+        final_board->boardState[7]->moved = false;
+    }
+    // IF BLACK ROOKS ARE CORRECT
+    if(final_board->boardState[56]->type == 'R'){
+        final_board->boardState[56]->moved = false;
+    } else if(final_board->boardState[63]->type == 'R'){
+        final_board->boardState[63]->moved = false;
+    }
+#pragma endregion SET_MOVED
+
 
     return final_board;
 }
